41164594ebf9f0254ccfa6ab77574d7931585b77
franekk3 <i41978152@gmail.com>
2026-03-15 11:32:29 +0100

    added source code
    source code of APK

---
+++ source/app/src/main/java/io/github/franekk3/Cubyy/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.franekk3.Cubyy;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
