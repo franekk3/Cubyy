2b01e43cf072d4354f3d7848c7020b72dcd307e6
franekk3 <i41978152@gmail.com>
2026-08-30 09:00:14 +0200

    1.2.3 Update

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-const CACHE_NAME = 'v1_cache';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = "cubyy-v1.2.3";
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request).then(response => {
+                    const responseClone = response.clone();
+
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+
+                    return response;
+                });
+            })
     );
 });
